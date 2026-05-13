bfc00982e1c10252adf9b0719389651a7dc7c40c
koichi <ducati_1919@yahoo.co.jp>
2026-05-13 21:15:19 +0900

    部屋詳細画像を各部屋に対応

--- rooms-data.js
+++ rooms-data.js
@@ -33,7 +33,7 @@ window.BANCHOU_ROOMS = [
   { no: 132, floor: 1, size: "約0.8帖 / 1.30㎡", tatami: 0.8, area: 1.30, price: 7150, vacant: false },
   { no: 133, floor: 1, size: "約2.7帖 / 4.40㎡", tatami: 2.7, area: 4.40, price: 24200, vacant: true },
   { no: 134, floor: 1, size: "約1.4帖 / 2.30㎡", tatami: 1.4, area: 2.30, price: 12650, vacant: false },
-  { no: 201, floor: 2, size: "約0.6帖 / 0.90㎡", tatami: 0.6, area: 0.90, price: 4050, vacant: false },
+  { no: 201, floor: 2, size: "\u7d040.6\u5e16 / 0.90\u33a1", tatami: 0.6, area: 0.90, price: 4050, vacant: false },
   { no: 202, floor: 2, size: "約0.6帖 / 1.00㎡", tatami: 0.6, area: 1.00, price: 4500, vacant: false },
   { no: 203, floor: 2, size: "約0.6帖 / 0.90㎡", tatami: 0.6, area: 0.90, price: 4050, vacant: false },
   { no: 204, floor: 2, size: "約0.5帖 / 0.80㎡", tatami: 0.5, area: 0.80, price: 3600, vacant: false },
@@ -48,3 +48,204 @@ window.BANCHOU_ROOMS = [
   { no: 213, floor: 2, size: "約1.7帖 / 2.80㎡", tatami: 1.7, area: 2.80, price: 12600, vacant: true },
   { no: 214, floor: 2, size: "約1.0帖 / 1.70㎡", tatami: 1.0, area: 1.70, price: 7650, vacant: false }
 ];
+
+window.BANCHOU_ROOM_IMAGES = {
+  101: [
+    "assets/101No1.JPG",
+    "assets/101No2.JPG"
+  ],
+  102: [
+    "assets/102No1.JPG",
+    "assets/102No2.JPG"
+  ],
+  103: [
+    "assets/103No1.JPG",
+    "assets/103No2.JPG"
+  ],
+  104: [
+    "assets/104No1.JPG",
+    "assets/104No2.JPG"
+  ],
+  105: [
+    "assets/105No1.JPG",
+    "assets/105No2.JPG"
+  ],
+  106: [
+    "assets/106No1.JPG",
+    "assets/106No2.JPG"
+  ],
+  107: [
+    "assets/107No1.JPG",
+    "assets/107No2.JPG"
+  ],
+  108: [
+    "assets/108No1.JPG",
+    "assets/108No2.JPG"
+  ],
+  109: [
+    "assets/109No1.JPG",
+    "assets/109No2.JPG"
+  ],
+  110: [
+    "assets/110No1.JPG",
+    "assets/110No2.JPG"
+  ],
+  111: [
+    "assets/111No1.JPG",
+    "assets/111No2.JPG"
+  ],
+  112: [
+    "assets/112No1.JPG",
+    "assets/112No2.JPG"
+  ],
+  113: [
+    "assets/113No1.JPG",
+    "assets/113No2.JPG",
+    "assets/113No3.JPG"
+  ],
+  114: [
+    "assets/114No1.JPG",
+    "assets/114No2.JPG"
+  ],
+  115: [
+    "assets/115No1.JPG",
+    "assets/115No2.JPG",
+    "assets/115No3.JPG"
+  ],
+  116: [
+    "assets/116No1.JPG",
+    "assets/116No2.JPG"
+  ],
+  117: [
+    "assets/117No1.JPG",
+    "assets/117No2.JPG"
+  ],
+  118: [
+    "assets/118No1.JPG",
+    "assets/118No2.JPG"
+  ],
+  119: [
+    "assets/119No1.JPG",
+    "assets/119No2.JPG"
+  ],
+  120: [
+    "assets/120No1.JPG",
+    "assets/120No2.JPG"
+  ],
+  121: [
+    "assets/121No1.JPG",
+    "assets/121No2.JPG"
+  ],
+  122: [
+    "assets/122No1.JPG",
+    "assets/122No2.JPG"
+  ],
+  123: [
+    "assets/123No1.JPG",
+    "assets/123No2.JPG"
+  ],
+  124: [
+    "assets/124No1.JPG",
+    "assets/124No2.JPG"
+  ],
+  125: [
+    "assets/125No1.JPG",
+    "assets/125No2.JPG"
+  ],
+  126: [
+    "assets/126No1.JPG",
+    "assets/126No2.JPG"
+  ],
+  127: [
+    "assets/127No1.JPG",
+    "assets/127No2.JPG"
+  ],
+  128: [
+    "assets/128No1.JPG",
+    "assets/128No2.JPG"
+  ],
+  129: [
+    "assets/129No1.JPG",
+    "assets/129No2.JPG"
+  ],
+  130: [
+    "assets/130No1.JPG",
+    "assets/130No2.jpg"
+  ],
+  131: [
+    "assets/131No1.JPG",
+    "assets/131No2.JPG"
+  ],
+  132: [
+    "assets/132No1.JPG",
+    "assets/132No2.JPG"
+  ],
+  133: [
+    "assets/133No1.JPG",
+    "assets/133No2.JPG"
+  ],
+  134: [
+    "assets/134No1.JPG",
+    "assets/134No2.jpg"
+  ],
+  201: [
+    "assets/201No1.JPG",
+    "assets/201No2.JPG"
+  ],
+  202: [
+    "assets/202 NO1.JPG",
+    "assets/202NO2.JPG"
+  ],
+  203: [
+    "assets/203NO1.JPG",
+    "assets/203NO2.JPG"
+  ],
+  204: [
+    "assets/204No1.JPG",
+    "assets/204No2.JPG"
+  ],
+  205: [
+    "assets/205No1.JPG",
+    "assets/205No2.JPG"
+  ],
+  206: [
+    "assets/206No1.JPG",
+    "assets/206No2.JPG"
+  ],
+  207: [
+    "assets/207No1.JPG",
+    "assets/207No2.JPG"
+  ],
+  208: [
+    "assets/208No2.JPG"
+  ],
+  209: [
+    "assets/209No1.JPG",
+    "assets/209No2.JPG"
+  ],
+  210: [
+    "assets/210No1.JPG",
+    "assets/210No2.JPG"
+  ],
+  211: [
+    "assets/211No1.JPG",
+    "assets/211No2.JPG"
+  ],
+  212: [
+    "assets/212No1.JPG",
+    "assets/212No2.JPG"
+  ],
+  213: [
+    "assets/213No1.JPG",
+    "assets/213No2.JPG"
+  ],
+  214: [
+    "assets/214No1.JPG",
+    "assets/214No2.JPG"
+  ]
+};
+
+window.BANCHOU_ROOMS.forEach(room => {
+  const images = window.BANCHOU_ROOM_IMAGES[room.no];
+  if (images) room.images = images;
+});
