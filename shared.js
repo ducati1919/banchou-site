930d2fc864204d8e1939ea508bc0e32e637cd2af
岩崎浩市 <iwasakikouichi@iwasakikouichinoMacBook-Air.local>
2026-07-18 23:21:57 +0900

    Fix proper 404 responses for unknown routes

---
+++ shared.js
@@ -0,0 +1,9 @@
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+
+if (toggle && nav) {
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    toggle.classList.toggle('open');
+  });
+}
